ea06b58e8f0f44d602aef3683f8febeb2dd66901
Saltzer <glebanton54@gmail.com>
2021-11-20 03:38:09 +0300

    Add files via upload

---
+++ exercises/variables/variables5.rs
@@ -0,0 +1,9 @@
+// variables5.rs
+// Make me compile! Execute the command `rustlings hint variables5` if you want a hint :)
+
+fn main() {
+    let number: &str = "T-H-R-E-E"; // don't change this line
+    let number: u32 = 3;
+    println!("Spell a Number : {}", number);
+    println!("Number plus two is : {}", number + 2);
+}
